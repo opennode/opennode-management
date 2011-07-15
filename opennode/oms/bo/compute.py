0e7404f8d62f2d3714d946a55dbdf94328b1818c
livenson <ilja.livenson@gmail.com>
2011-07-15 21:31:33 +0300

    business object sample

---
+++ opennode/oms/bo/compute.py
@@ -0,0 +1,11 @@
+""" Operations on compute instances. """
+from opennode.oms.model.compute import Compute
+from twisted.internet.defer import Deferred
+
+def get_compute_basic(id):
+    # XXX call to DB
+    return Deferred()
+
+def get_compute_full(id):
+    # XXX call to DB
+    return Deferred()
