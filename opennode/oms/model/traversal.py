efa56c774d5bd738fab97769151beab948195c2d
Erik Allik <eallik@gmail.com>
2011-07-23 14:18:35 +0300

    Added model.traversal.traverse_path for traversing paths of arbitrary complexity

--- opennode/oms/model/traversal.py
+++ opennode/oms/model/traversal.py
@@ -1,5 +1,7 @@
 from zope.interface import Interface, implements
 
+from opennode.oms import db
+
 
 class ITraverser(Interface):
     def traverse(name, store):
@@ -11,3 +13,37 @@ class Traverser(object):
 
     def __init__(self, context):
         self.context = context
+
+
+def traverse_path(obj, path):
+    """Using the given store, traverses the objects in the
+    database to find an object that matches the given path.
+
+    Returns the object up to which the traversal was successful,
+    and the part of the path that could not be resolved.
+
+    """
+
+    if not path:
+        return obj
+
+    store = db.get_store()
+
+    path = path.split('/')
+
+    # Allow one extra slash at the end:
+    if  path[-1] == '': path.pop()
+
+    ret = [obj]
+    while path:
+        name = path[0]
+        traverser = ITraverser(ret[-1])
+
+        next_obj = traverser.traverse(name, store=store)
+
+        if not next_obj: break
+
+        ret.append(next_obj)
+        path = path[1:]
+
+    return ret[1:], path
