3bd458d257ad9a6ed4901cf171b0ae0dba41b4ae
Erik Allik <eallik@gmail.com>
2011-07-23 19:35:36 +0300

    Updated and cleaned up model.tests.basic

--- opennode/oms/model/tests/basic.py
+++ opennode/oms/model/tests/basic.py
@@ -1,45 +1,45 @@
 import unittest
 
-from opennode.oms.db import DB_URI
 from storm.database import create_database
 from storm.store import Store
+
+from opennode.oms.db.db import DB_URI
 from opennode.oms.model.model import Compute, Network, NetworkDevice
+
 
 class Test(unittest.TestCase):
 
-
     def setUp(self):
-        self.db = create_database(DB_URI)
-        self.s = Store(self.db)
+        db = create_database(DB_URI)
+        self.store = Store(db)
 
     def tearDown(self):
-        self.s.rollback()
-        self.s.close()
-
+        self.store.rollback()
+        self.store.close()
 
     def testSimpleCreation(self):
-        c = Compute()
-        c.hostname = u'test.host'
-        self.s.add(c)
+        compute = Compute()
+        compute.hostname = u'test.host'
+        self.store.add(compute)
 
-        n = Network()
-        n.ipv4_address_range = u'192.168.1.0/24'
-        self.s.add(n)
+        network = Network()
+        network.ipv4_address_range = u'192.168.1.0/24'
+        self.store.add(network)
 
-        nd = NetworkDevice()
-        nd.mac = u'aa:bb:cc:dd:ee:ee'
-        nd.compute = c
-        nd.network = n
-        self.s.add(nd)
+        network_device = NetworkDevice()
+        network_device.mac = u'aa:bb:cc:dd:ee:ee'
+        network_device.compute = compute
+        network_device.network = network
+        self.store.add(network_device)
 
-        nd = NetworkDevice()
-        nd.mac = u'aa:bb:cc:dd:ee:ff'
-        nd.network = n
-        self.s.add(nd)
+        network_device = NetworkDevice()
+        network_device.mac = u'aa:bb:cc:dd:ee:ff'
+        network_device.network = network
+        self.store.add(network_device)
 
-        self.s.flush()
-        self.assertEquals(c.network_devices.count(), 1)
-        self.assertEquals(n.devices.count(), 2)
+        self.store.flush()
+        self.assertEquals(compute.network_devices.count(), 1)
+        self.assertEquals(network.devices.count(), 2)
 
 
 if __name__ == "__main__":
