0139d26f3474422a8e3ad7e0fe7081780d2eef0e
Erik Allik <eallik@gmail.com>
2011-08-09 00:57:31 +0300

    Cleaned up and simplified backend/func.py

--- opennode/oms/backend/func.py
+++ opennode/oms/backend/func.py
@@ -1,19 +1,18 @@
 from __future__ import absolute_import
-from zope.interface import classImplements
 
+from func import jobthing
+from func.overlord.client import Overlord
 from grokcore.component import Adapter, context, baseclass
 from twisted.internet import reactor
-from func.overlord.client import Overlord
-from func import jobthing
+from zope.interface import classImplements
 
-from opennode.oms.backend.operation import IFuncInstalled
-from opennode.oms.backend.operation import IGetComputeInfo
+from opennode.oms.backend.operation import IFuncInstalled, IGetComputeInfo
 
 
 class FuncBase(Adapter):
     """Base class for all Func method calls."""
     context(IFuncInstalled)
-    baseclass() # to avoid grokking of this class 
+    baseclass()
 
     func_action = None
 
@@ -29,26 +28,28 @@ class FuncBase(Adapter):
 
     def start_polling(self):
         return_code, results = self._get_client().job_status(self.job_id)
-        if return_code in(jobthing.JOB_ID_FINISHED, jobthing.JOB_ID_REMOTE_ERROR):
+        if return_code in (jobthing.JOB_ID_FINISHED, jobthing.JOB_ID_REMOTE_ERROR):
             self._fire_events(results)
             return
-        reactor.callLater(1, self.start_polling) 
+        reactor.callLater(1, self.start_polling)
 
     def _fire_events(self, data):
         # XXX: Sending signal to self.context
         pass
 
     def _get_client(self):
-        """Return an instance of the Overlord."""
+        """Returns an instance of the Overlord."""
         return Overlord(self.context.hostname, async=True)
 
-FUNC_ACTIONS = {IGetComputeInfo: 'hardware.info'}
-FUNC_CLASSES = {}
 
-# iterate over supported func functions creating a new class with a correct Func action
+FUNC_ACTIONS = {IGetComputeInfo: 'hardware.info'}
+
+
+# iterate over supported func functions creating a new class with the correct Func action
 for interface, action in FUNC_ACTIONS.items():
     cls_name = 'Func%s' % interface.__name__[1:]
-    FUNC_CLASSES[cls_name] = type(cls_name, (FuncBase, ), dict(func_action=action))
-    globals()[cls_name] = FUNC_CLASSES[cls_name]
-    classImplements(FUNC_CLASSES[cls_name], interface)
+    cls = type(cls_name, (FuncBase, ), dict(func_action=action))
+    classImplements(cls, interface)
+    globals()[cls_name] = cls
 
+del cls
