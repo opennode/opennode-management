57280a13e110f1a48c356787c470563bca1adf8a
Erik Allik <eallik@gmail.com>
2011-08-09 15:02:06 +0300

    Renamed backends/func/actions.py back to backends/func.py

--- opennode/oms/tests/test_func.py
+++ opennode/oms/tests/test_func.py
@@ -2,7 +2,7 @@ from zope.interface import alsoProvides
 
 from opennode.oms.backend.operation import IFuncInstalled, IGetComputeInfo
 from opennode.oms.model.model.compute import Compute
-from opennode.oms.backend.func.actions import FuncGetComputeInfo
+from opennode.oms.backend.func import FuncGetComputeInfo
 
 
 def test_adaption():
