6d86c1c71652e8f0fceae1cb273b4e4b678b75c4
Erik Allik <eallik@gmail.com>
2011-08-13 19:30:43 +0300

    Added optional arguments to test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-nosetests --nologcapture --detailed-errors --nocapture
+#!/bin/bash
+
+if [ ! -z "$1" ] ; then
+    arg1="opennode.oms.tests.test_$1"
+fi
+
+if [ ! -z "$2" ] ; then
+    arg2="opennode.oms.tests.test_$2"
+fi
+
+if [ ! -z "$3" ] ; then
+    arg3="opennode.oms.tests.test_$3"
+fi
+
+nosetests --nologcapture --detailed-errors --nocapture $arg1 $arg2 $arg3
