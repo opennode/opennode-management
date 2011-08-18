opennode/opennode-management

commit 6ad32873fd88527db1a6431eb8c570ebdd5a1619

Fixed handling of '--help' which caused commands to force their mandatory parameters to be optional and manually check for them and report usage.

opennode/oms/endpoint/ssh/cmdline.py
@@ -58,15 +58,7 @@ class VirtualConsoleArgumentParser(InstrumentableArgumentParser):
         super(VirtualConsoleArgumentParser, self).__init__(add_help=False, formatter_class=VirtualConsoleHelpFormatter, *args, **kwargs)
 
         if add_help:
-            self.add_argument('-h', '--help', action='store_true', default=argparse.SUPPRESS, help="show this help message and exit")
-
-    def parse_args(self, args=None, namespace=None):
-        args = super(VirtualConsoleArgumentParser, self).parse_args(args, namespace)
-        if getattr(args, 'help', None):
-            self.print_help()
-            # XXX: or shall we go back and use
-            raise ArgumentParsingInterrupted
-        return args
+            self.add_argument('-h', '--help', action=argparse._HelpAction, help="show this help message and exit")
 
 
 class PartialVirtualConsoleArgumentParser(VirtualConsoleArgumentParser):

opennode/oms/endpoint/ssh/completion_cmds.py
@@ -60,9 +60,11 @@ class PathCompleter(Completer):
                     actual += len(values)
 
                     # And the maximum number of expected occurencies.
-                    if isinstance(action.nargs, int):
+                    if action.nargs is None:
+                        maximum += 1
+                    elif isinstance(action.nargs, int):
                         maximum += action.nargs
-                    if action.nargs == argparse.OPTIONAL:
+                    elif action.nargs == argparse.OPTIONAL:
                         maximum += 1
                     else:
                         maximum = float('inf')

opennode/oms/tests/test_completion.py
@@ -90,9 +90,8 @@ class CmdCompletionTestCase(unittest.TestCase):
         eq_(self.terminal.method_calls, [('write', ('help ',), {})])
 
     def test_complete_consumed_switches(self):
-        self._tab_after('ls --help')
+        self._tab_after('ls -l')
         eq_(self.terminal.method_calls, [('write', (' ',), {})])
 
         self._tab_after('-')
-        assert 'help' not in self.terminal.method_calls[2][1][0]
-        assert '-h' not in self.terminal.method_calls[2][1][0]
+        assert '-l' not in self.terminal.method_calls[2][1][0]

commit 0a34d1f6311f5521b67c4fba9cf0a001c09c5157

Fixed help for contextualized commands. Now the help is context dependend again, so `set compute/1 -h` will show all valid attributes.
Issue introduced in 6ad32873 when help was reverted to raise an exception during parse.

opennode/oms/endpoint/ssh/cmd.py
@@ -64,7 +64,9 @@ class Cmd(object):
         contextual = queryAdapter(self, IContextualCmdArgumentsSyntax)
         if contextual:
             try:
-                parsed, rest = parser.parse_known_args(args)
+                # Inhibit the help action from interrupting our partial parse
+                # since we want the contextualized parser to be built also for printing help/usage.
+                parsed, rest = parser.parse_known_args(args, argparse.Namespace(inhibit_help=True))
             except ArgumentParsingError:
                 # Fall back to uncontextualied parsed in case of parsing errors.
                 # This happens when the "context defining" argument is declared as mandatory

opennode/oms/endpoint/ssh/cmdline.py
@@ -58,7 +58,7 @@ class VirtualConsoleArgumentParser(InstrumentableArgumentParser):
         super(VirtualConsoleArgumentParser, self).__init__(add_help=False, formatter_class=VirtualConsoleHelpFormatter, *args, **kwargs)
 
         if add_help:
-            self.add_argument('-h', '--help', action=argparse._HelpAction, help="show this help message and exit")
+            self.add_argument('-h', '--help', action=HelpAction, help="show this help message and exit")
 
 
 class PartialVirtualConsoleArgumentParser(VirtualConsoleArgumentParser):
@@ -94,6 +94,12 @@ class VirtualConsoleHelpFormatter(argparse.HelpFormatter):
     def format_help(self):
         help = super(VirtualConsoleHelpFormatter, self).format_help()
         return re.sub(r'=(\w*)', r'\1 =', help)
+
+
+class HelpAction(argparse._HelpAction):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not getattr(namespace, 'inhibit_help', None):
+            return super(HelpAction, self).__call__(parser, namespace, values, option_string)
 
 
 class GroupDictAction(argparse.Action):

opennode/oms/tests/test_ssh.py
@@ -101,6 +101,16 @@ class SshTestCase(unittest.TestCase):
             ('write', ('State:          \tactive\n',)),
         ]
 
+    @run_in_reactor
+    def test_contextualized_help(self):
+        computes = db.get_root()['oms_root']['computes']
+        computes.add(Compute('linux', 'tux-for-test', 2000, 2000, 'active'))
+
+        self.terminal.reset_mock()
+        self._cmd('set computes/1 -h')
+
+        assert 'hostname = ' in self.terminal.method_calls[0][1][0]
+
     def test_tokenizer(self):
         arglist = 'set /computes/some\\ file\\ \\ with\\ spaces -v --help key=value other_key="quoted value" "lastkey"="escaped \\" quotes"'
 