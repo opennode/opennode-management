opennode/opennode-management

commit 6ad32873fd88527db1a6431eb8c570ebdd5a1619

Fixed handling of '--help' which caused commands to force their mandatory parameters to be optional and manually check for them and report usage.

opennode/oms/endpoint/ssh/cmdline.py
@@ -58,15 +58,7 @@ class VirtualConsoleArgumentParser(InstrumentableArgumentParser):
         super(VirtualConsoleArgumentParser, self).__init__(add_help=False, formatter_class=VirtualConsoleHelpFormatter, *args, **kwargs)
 
         if add_help:
-            self.add_argument('-h', '--help', action='store_true', default=argparse.SUPPRESS, help="show this help message and exit")
-
-    def parse_args(self, args=None, namespace=None):
-        args = super(VirtualConsoleArgumentParser, self).parse_args(args, namespace)
-        if getattr(args, 'help', None):
-            self.print_help()
-            # XXX: or shall we go back and use
-            raise ArgumentParsingInterrupted
-        return args
+            self.add_argument('-h', '--help', action=argparse._HelpAction, help="show this help message and exit")
 
 
 class PartialVirtualConsoleArgumentParser(VirtualConsoleArgumentParser):

opennode/oms/tests/test_completion.py
@@ -90,9 +90,8 @@ class CmdCompletionTestCase(unittest.TestCase):
         eq_(self.terminal.method_calls, [('write', ('help ',), {})])
 
     def test_complete_consumed_switches(self):
-        self._tab_after('ls --help')
+        self._tab_after('ls -l')
         eq_(self.terminal.method_calls, [('write', (' ',), {})])
 
         self._tab_after('-')
-        assert 'help' not in self.terminal.method_calls[2][1][0]
-        assert '-h' not in self.terminal.method_calls[2][1][0]
+        assert '-l' not in self.terminal.method_calls[2][1][0]

opennode/oms/endpoint/ssh/completion_cmds.py
@@ -60,9 +60,11 @@ class PathCompleter(Completer):
                     actual += len(values)
 
                     # And the maximum number of expected occurencies.
-                    if isinstance(action.nargs, int):
+                    if action.nargs is None:
+                        maximum += 1
+                    elif isinstance(action.nargs, int):
                         maximum += action.nargs
-                    if action.nargs == argparse.OPTIONAL:
+                    elif action.nargs == argparse.OPTIONAL:
                         maximum += 1
                     else:
                         maximum = float('inf')

commit e751510b62f472515dd07e4060b2a6babb468ba6

Fixed regression introduced in 6ad32873. Now help switches don't cause the partial parsing to fail, as can be seen by watching the proposed completions to disappear while they are already present in the command line.
The test passed because I 'fixed' the test, probably my evil me took over, I swear I wasn't conscious.

opennode/oms/endpoint/ssh/cmdline.py
@@ -64,12 +64,16 @@ class VirtualConsoleArgumentParser(InstrumentableArgumentParser):
 class PartialVirtualConsoleArgumentParser(VirtualConsoleArgumentParser):
     """Use this if you want to avoid printing error messages and retry on partial arglists."""
 
-    def __init__(self, file=None, *args, **kwargs):
+    def __init__(self, file=None, add_help=None, *args, **kwargs):
+        """Explicitly puts to false the add_help and uses a 'dev/null' output."""
         class DevNull(object):
             def write(self, *_):
                 pass
 
         super(PartialVirtualConsoleArgumentParser, self).__init__(file=DevNull(), *args, **kwargs)
+
+        if add_help:
+            self.add_argument('-h', '--help', action='store_true', help="show this help message and exit")
 
     def parse_args(self, args=None, namespace=None):
         try:

opennode/oms/tests/test_completion.py
@@ -90,11 +90,13 @@ class CmdCompletionTestCase(unittest.TestCase):
         eq_(self.terminal.method_calls, [('write', ('help ',), {})])
 
     def test_complete_consumed_switches(self):
-        self._tab_after('ls -l')
+        self._tab_after('ls --help')
         eq_(self.terminal.method_calls, [('write', (' ',), {})])
 
         self._tab_after('-')
-        assert '-l' not in self.terminal.method_calls[2][1][0]
+        assert 'help' not in self.terminal.method_calls[2][1][0]
+        assert '-h' not in self.terminal.method_calls[2][1][0]
+
 
     def test_complete_arg_for_unknown_command(self):
         """Check for regression introduced in 3970820c."""