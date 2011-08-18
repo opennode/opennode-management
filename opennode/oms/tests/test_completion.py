opennode/opennode-management

commit 3970820c818b15583d67154a1e6d28a565c66c74

Simplified undefined_command

opennode/oms/endpoint/ssh/cmd.py
@@ -101,25 +101,6 @@ class Cmd(object):
         else:
             return objs[-1]
 
-def undefined_command(cmd_name):
-    """Creates a temporary command class which complains about a missing command.
-    Currently OmsSshProtocol needs to needs to get a command class, like every other command,
-    and it will instantiate it.
-
-    We could return a function which then constructs a UndefinedCommand passing the cmd_name as constructor,
-    but dynamically creating classes like this is cheap in python,
-    so let's leverage the fact that cmd_name is in the lexical scope.
-    """
-
-    class UndefinedCommand(Cmd):
-        """Represents an undefined command."""
-
-        def __call__(self, *args):
-            """Report an error"""
-            self.terminal.write('No such command: %s' % cmd_name)
-            self.terminal.nextLine()
-    return UndefinedCommand
-
 
 class NoCommand(Cmd):
     """Represents the fact that there is no command yet."""
@@ -409,7 +390,14 @@ def get_command(name):
 
     """
 
+    # TODO: Remove this once we have the better command registration (described above).
     if not name:
         return NoCommand
 
-    return commands().get(name, undefined_command(name))
+    # TODO: Is this approach needed as opposed to handling it
+    # upstream? Is this a result of over engineering?
+    class UndefinedCommand(Cmd):
+        def __call__(self, *args):
+            self.terminal.write("No such command: %s\n" % name)
+
+    return commands().get(name, UndefinedCommand)

commit 4a87672f8fa1531e3acdf8613be354842d46713c

Fixed completion regression introduced in 3970820c. With test case.

opennode/oms/endpoint/ssh/cmd.py
@@ -448,4 +448,6 @@ def get_command(name):
         def __call__(self, *args):
             self.terminal.write("No such command: %s\n" % name)
 
+    UndefinedCommand.name = name
+
     return commands().get(name, UndefinedCommand)

opennode/oms/tests/test_completion.py
@@ -95,3 +95,34 @@ class CmdCompletionTestCase(unittest.TestCase):
 
         self._tab_after('-')
         assert '-l' not in self.terminal.method_calls[2][1][0]
+
+    def test_complete_arg_for_unknown_command(self):
+        """Check for regression introduced in 3970820c."""
+
+        # NOTE: this monkeypatch is used to  catch an exception which would happen inside a deferred?
+        # is there a better way to check for an exception inside a deferred during tests?
+        # otherwise this unit test in 3970820c would raise an exception in deferred
+        # outputing to stderr, but the unit test would pass.
+
+        got_exception = [None]
+        old_name = cmd.Cmd.name
+        class Dummy(object):
+            @property
+            def catch_it(self):
+                print "RUNNING CATCHIT"
+                try:
+                    return old_name.__get__(self)
+                except Exception as e:
+                    got_exception[0] = e
+                    raise e
+        cmd.Cmd.name = Dummy.catch_it
+
+        # actual test
+        try:
+            self._tab_after('junk ')
+            eq_(self.terminal.method_calls, [])
+
+            assert not got_exception[0]
+        finally:
+            # cleanup the monkeypath
+            cmd.Cmd.name = old_name