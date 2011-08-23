opennode/opennode-management

commit 2b7e3e3bf76e173a5f4ab26e61232c26beefe949

Made ssh.cmds.mk print out the ID of the newly created object

opennode/oms/endpoint/ssh/cmd/commands.py
@@ -1,3 +1,4 @@
+import inspect
 import transaction
 import zope.schema
 from grokcore.component import implements, context, Adapter, Subscription, baseclass, order
@@ -368,10 +369,11 @@ class CreateObjCmd(Cmd):
         # like the apply_raw_data
         # NOTE: arparser already convers int parameters according to the zope.schema.
         # but we might want to create nodes also from other APIs, so something like apply_raw_data would fit.
-        import inspect
         obj = model_cls(*[args.keywords.get(arg_name, None) for arg_name in inspect.getargspec(model_cls.__init__).args[1:]])
 
-        self.current_obj.add(obj)
+        obj_id = self.current_obj.add(obj)
+
+        self.write(str(obj_id))
 
 
 class MkCmdDynamicArguments(Adapter):

commit e336f7d875c0ca5845ca331744e9b8d9439de8a8

Fixed minor rendering issue of `mk` command output introduced in 2b7e3e3bf76e17.

opennode/oms/endpoint/ssh/cmd/commands.py
@@ -373,7 +373,7 @@ class CreateObjCmd(Cmd):
 
         obj_id = self.current_obj.add(obj)
 
-        self.write(str(obj_id))
+        self.write("%s\n" % obj_id)
 
 
 class MkCmdDynamicArguments(Adapter):