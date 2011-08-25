63a17cfb2f7b55bb40746544a8afe736938d025a
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-08-26 01:24:20 +0200

    Added suport for interactive password auth in case pubkey fails.

--- opennode/oms/endpoint/webterm/ssh.py
+++ opennode/oms/endpoint/webterm/ssh.py
@@ -1,36 +1,50 @@
 import os
 
 from twisted.conch.ssh import transport, keys, userauth, connection, channel, session
-from twisted.internet import defer
+from twisted.internet import defer, reactor, protocol
 
 
-class ClientTransport(transport.SSHClientTransport):
+def ssh_connect_interactive_shell(user, host, port, transport, set_channel, size):
+    protocol.ClientCreator(reactor, SSHClientTransport, transport, set_channel, size, user, host).connectTCP(host, port)
 
-    def __init__(self, terminal_transport, set_channel, terminal_size):
+class SSHClientTransport(transport.SSHClientTransport):
+    """Performs a SSH connection to a server."""
+
+    def __init__(self, terminal_transport, set_channel, terminal_size, user, host):
         self.terminal_transport = terminal_transport
         self.set_channel = set_channel
         self.terminal_size = terminal_size
+        self.user = user
+        self.host = host
 
     def verifyHostKey(self, pubKey, fingerprint):
         # TODO: check fingerprints?
         return defer.succeed(1)
 
     def connectionSecure(self):
-        self.requestService(ClientUserAuth(os.environ["USER"], ClientConnection(self.terminal_transport, self.set_channel, self.terminal_size)))
+        self.requestService(ClientUserAuth(self.user, SSHShellConnection(self.terminal_transport, self.set_channel, self.terminal_size)))
 
 
 class ClientUserAuth(userauth.SSHUserAuthClient):
+    """Performs ssh connection authentication on behalf of a SSHClientTransport.
+
+    Public key authentication is designed to be performed with the OMS service key.
+    The OMS thus takes the responsibility to authorize a given principal and then
+    logs in on his behalf.
+
+    """
 
     def __init__(self, *args, **kwargs):
+        """Read the pub/priv keys from the standard location of the OMS home directory."""
         # Super is old-style class
         userauth.SSHUserAuthClient.__init__(self, *args, **kwargs)
 
-        names = ["id_fake_dsa", "id_fake_rsa", "id_dsa", "id_rsa"]
+        names = ['id_oms_dsa', 'id_oms_rsa', 'id_fake_dsa', 'id_fake_rsa', 'id_dsa', 'id_rsa']
 
         def read(ext):
-            home = os.environ["HOME"]
+            home = os.environ['HOME']
             for base in names:
-                name = "%s/.ssh/%s%s" % (home, base, ext)
+                name = '%s/.ssh/%s%s' % (home, base, ext)
                 if os.path.exists(name):
                     f = open(name, 'r')
                     return f.read()
@@ -39,18 +53,50 @@ class ClientUserAuth(userauth.SSHUserAuthClient):
         self.publicKey = read('.pub')
         self.privateKey = read('')
 
-    def getPassword(self, prompt = None):
-        # this says we won't do password authentication
-        return
-
     def getPublicKey(self):
+        """Return the public key unless we already failed our previous publickey auth attempt."""
+        if self.lastAuth == 'publickey':
+            return None
         return keys.Key.fromString(data = self.publicKey).blob()
 
     def getPrivateKey(self):
         return defer.succeed(keys.Key.fromString(data = self.privateKey).keyObject)
 
 
-class ClientConnection(connection.SSHConnection):
+    def getPassword(self, prompt = None):
+        """Conch expects a deferred which will yield a password to try.
+        Writes a prompt and redirect the terminal input to a password reader which will
+        callback a deferred and thus yield a password to conch.
+
+        The terminal input will be automatically handled over to the ssh channel
+        after successful authentication.
+
+        """
+
+        terminal = self.transport.terminal_transport
+        terminal.write("%s@%s's password: " % (self.user, self.transport.host))
+
+        deferred_password = defer.Deferred()
+
+        class PasswordReader(object):
+            def __init__(self):
+                self.password = ""
+
+            def write(self, ch):
+                if ch == '\x7f':
+                    self.password = self.password[:-1]
+                elif ch == '\r':
+                    deferred_password.callback(self.password)
+                    terminal.write('\r\n')
+                else:
+                    self.password += ch
+
+        self.transport.set_channel(PasswordReader())
+        return deferred_password
+
+
+class SSHShellConnection(connection.SSHConnection):
+    """Represents a SSH client connection opening a interactive remote shell."""
 
     def __init__(self, terminal_transport, set_channel, terminal_size):
         connection.SSHConnection.__init__(self)
@@ -60,17 +106,19 @@ class ClientConnection(connection.SSHConnection):
         self.terminal_size = terminal_size
 
     def serviceStarted(self):
-        self.openChannel(ShellChannel(terminal_transport=self.terminal_transport, conn = self))
+        self.openChannel(ShellChannel(conn = self))
 
 
 class ShellChannel(channel.SSHChannel):
+    """This is the core of the interactive ssh shell.
 
+    """
     name = 'session'
 
-    def __init__(self, terminal_transport=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         # Super is old-style class
         channel.SSHChannel.__init__(self, *args, **kwargs)
-        self.terminal_transport = terminal_transport
+        self.terminal_transport = self.conn.terminal_transport
 
     def openFailed(self, reason):
         print 'Channel failed', reason
