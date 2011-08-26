19928206f5e5dfccf905517469500bf5b19320fe
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-08-26 06:07:31 +0200

    WIP: Cleanups and refactorings

--- opennode/oms/endpoint/webterm/root.py
+++ opennode/oms/endpoint/webterm/root.py
@@ -4,34 +4,12 @@ import time
 import uuid
 
 from twisted.conch.insults.insults import ServerProtocol
-from twisted.internet import defer, reactor
+from twisted.internet import reactor
 from twisted.web import resource
 from twisted.web.server import NOT_DONE_YET
 
 from opennode.oms.endpoint.ssh.protocol import OmsSshProtocol
 from opennode.oms.endpoint.webterm.ssh import ssh_connect_interactive_shell
-
-
-class WebTransport(object):
-    """Used by WebTerminal to actually send the data through the http transport."""
-
-    def __init__(self, session):
-        self.session = session
-
-    def write(self, text):
-        # Group together writes so that we reduce the number of http roundtrips.
-        # Kind of Nagle's algorithm.
-        self.session.buffer += text
-        reactor.callLater(0.05, self.session.processQueue)
-
-
-class WebTerminal(ServerProtocol):
-    """Used by TerminalProtocols (like OmsSshProtocol) to actually manipulate the terminal."""
-
-    def __init__(self, session):
-        ServerProtocol.__init__(self)
-        self.session = session
-        self.transport = WebTransport(session)
 
 
 class OmsShellTerminalProtocol(object):
@@ -67,6 +45,28 @@ class SSHClientTerminalProtocol(object):
         self.channel.write(key)
 
 
+class WebTransport(object):
+    """Used by WebTerminal to actually send the data through the http transport."""
+
+    def __init__(self, session):
+        self.session = session
+
+    def write(self, text):
+        # Group together writes so that we reduce the number of http roundtrips.
+        # Kind of Nagle's algorithm.
+        self.session.buffer += text
+        reactor.callLater(0.05, self.session.processQueue)
+
+
+class WebTerminal(ServerProtocol):
+    """Used by TerminalProtocols (like OmsSshProtocol) to actually manipulate the terminal."""
+
+    def __init__(self, session):
+        ServerProtocol.__init__(self)
+        self.session = session
+        self.transport = WebTransport(session)
+
+
 class TerminalSession(object):
     """A session for our ajax terminal emulator."""
 
@@ -95,9 +95,14 @@ class TerminalSession(object):
         for key in self.parse_keys(key_stream):
             self.terminal_protocol.handle_key(key)
 
+    def enqueue(self, request):
+        self.queue.append(request)
+        if self.buffer:
+            self.processQueue()
+
     def processQueue(self):
-        # Teoretically only one ongoing polling request should be live.
-        # but I'm not sure if this can be guaranteed so let's keep them all.
+        # Only one ongoing polling request should be live.
+        # But I'm not sure if this can be guaranteed so let's keep temporarily keep them all.
         if self.queue:
             for r in self.queue:
                 self.write(r)
@@ -122,7 +127,9 @@ class TerminalSession(object):
 
 
 class TerminalServer(resource.Resource):
-    """Web resource whic creates web terminal sessions."""
+    """Web resource which handles web terminal sessions adhering to ShellInABox.js protocol.
+
+    """
 
     def render_OPTIONS(self, request):
         """Return headers which allow cross domain xhr for this."""
@@ -153,8 +160,12 @@ class TerminalServer(resource.Resource):
         if not session_id:
             size = (int(request.args['width'][0]), int(request.args['height'][0]))
             session = TerminalSession(self.terminal_protocol, size)
+            session_id = session.id
             self.sessions[session.id] = session
-            return json.dumps(dict(session=session.id, data=session.buffer))
+
+        if not self.sessions.has_key(session_id):
+            request.setResponseCode(500)
+            return "no such session " + session_id
 
         session = self.sessions[session_id]
 
@@ -164,11 +175,9 @@ class TerminalServer(resource.Resource):
         keys = request.args.get('keys', None)
         if keys:
             session.handle_keys(keys[0])
-            return ""
+            return "" # responsed to this kind of requests are ignored
         else:
-            session.queue.append(request)
-            if session.buffer:
-                defer.maybeDeferred(session.processQueue)
+            session.enqueue(request)
 
         return NOT_DONE_YET
 
