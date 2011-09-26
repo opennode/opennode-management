6f8b19a8e4293b259ebe38e1d103e6c9e7a5b4f8
Erik Allik <eallik@gmail.com>
2011-09-26 19:52:52 +0300

    Made Compute.ip_address not required

--- opennode/oms/model/model/compute.py
+++ opennode/oms/model/model/compute.py
@@ -9,7 +9,7 @@ from .base import Model, Container
 class ICompute(Interface):
     architecture = schema.Choice(title=u"Architecture", values=(u'linux', u'win32', u'darwin', u'bsd', u'solaris'))
     hostname = schema.TextLine(title=u"Host name", min_length=3)
-    ip_address = schema.TextLine(title=u"IP address", min_length=7)
+    ip_address = schema.TextLine(title=u"IP address", min_length=7, required=False)
     speed = schema.Int(title=u"CPU Speed", description=u"CPU Speed in MHz")
     memory = schema.Int(title=u"RAM Size", description=u"RAM size in MB")
     state = schema.Choice(title=u"State", values=(u'active', u'inactive', u'standby'))
@@ -18,7 +18,7 @@ class ICompute(Interface):
 class Compute(Model):
     implements(ICompute)
 
-    ip_address = '0.0.0.0'
+    ip_address = u'0.0.0.0'
     type = 'unknown'  # XXX: how should this be determined?
                       # and how do we differentiate for ONC physical and virtual computes?
     cpu = "Intel Xeon 12.2GHz"
@@ -34,14 +34,15 @@ class Compute(Model):
     diskspace_backuppartition = 77.7
     startup_timestamp = "2011-07-06 01:23:45"
 
-    def __init__(self, architecture, hostname, ip_address, speed, memory, state, template=None):
+    def __init__(self, architecture, hostname, speed, memory, state, template=None, ip_address=None):
         self.architecture = architecture
         self.hostname = hostname
-        self.ip_address = ip_address
         self.speed = speed
         self.memory = memory
         self.state = state
         self.template = template
+        if ip_address:
+            self.ip_address = ip_address
 
     @property
     def nicknames(self):
