opennode/opennode-management

commit e946ef6f0a25bd7e1425748facb13e61d6487ffa

Fixed terminal mess after reset. Also useful when attaching nested consoles.

opennode/oms/endpoint/ssh/terminal.py
@@ -56,6 +56,8 @@ class InteractiveTerminal(recvline.HistoricRecvLine):
         self.found_index = -1
         self.search_skip = 0
 
+        self.terminal.reset()
+
     def set_terminal(self, terminal):
         self.terminal = terminal
         terminal.terminalProtocol = self

commit a8d2029c0e018424ae300eaac043ab6123c4aad6

Fixed regression introduced in e946ef6, which left the terminal in overwrite mode

opennode/oms/endpoint/ssh/terminal.py
@@ -2,6 +2,7 @@ import os
 import string
 
 from twisted.conch import recvline
+from twisted.conch.insults.insults import modes
 from twisted.python import log
 
 
@@ -57,6 +58,7 @@ class InteractiveTerminal(recvline.HistoricRecvLine):
         self.search_skip = 0
 
         self.terminal.reset()
+        self.terminal.setModes((modes.IRM, ))
 
     def set_terminal(self, terminal):
         self.terminal = terminal