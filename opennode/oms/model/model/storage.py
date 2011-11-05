231e9c02e00f13fded36ca2427f40cd940a019eb
Ilja Livenson <ilja.livenson@gmail.com>
2011-11-05 11:32:26 +0100

    storage fields added to a draft model

--- opennode/oms/model/model/storage.py
+++ opennode/oms/model/model/storage.py
@@ -5,5 +5,11 @@ from .base import Model
 
 class Storage(Model):
     def __init__(self, size, state):
-        self.size = size  # 1.1 GiB
+        self.id = 1 # UID
+	self.name = "Local storage" # description
+	self.path = '/storagemount/local'
+	self.type = local # local | shared 
+	self.size = size  # 1.1 GiB
+	self.used_size = 0 
         self.state = state  # online | offline | backup | snapshot | resize | degraded
+	
