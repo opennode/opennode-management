opennode/opennode-management

commit 50337bc1d3b2b7a028769d780459251be900b7f9

Only allow GET requests by default as opposed to falling back to the GET handler in case of unimplemented methods

opennode/oms/endpoint/httprest/view.py
@@ -23,7 +23,7 @@ from opennode.oms.util import get_direct_interfaces
 class DefaultView(HttpRestView):
     context(object)
 
-    def render(self, request):
+    def render_GET(self, request):
         obj = self.context
         # NODE: code copied from commands.py:CatCmd
         schemas = get_direct_interfaces(obj)
@@ -59,7 +59,7 @@ class DefaultView(HttpRestView):
 class ContainerView(DefaultView):
     context(IContainer)
 
-    def render(self, request):
+    def render_GET(self, request):
         depth = request.args.get('depth', ['0'])[0]
         try:
             depth = int(depth)

commit f34f5b3d593ad3f57b8b2d2559973fb5b351bb7d

Fixed additional issues created with the rename of the render method introduced in 50337bc1d3b2b7, related to ticket #61. We really need unit tests for rest output.

opennode/oms/endpoint/httprest/base.py
@@ -15,4 +15,4 @@ class HttpRestView(Adapter):
     baseclass()
 
     def render_recursive(self, request, depth):
-        return self.render(request)
+        return self.render_GET(request)

opennode/oms/endpoint/httprest/view.py
@@ -120,7 +120,7 @@ class VirtualizationContainerView(ContainerView):
 class ComputeView(HttpRestView):
     context(Compute)
 
-    def render(self, request):
+    def render_GET(self, request):
 
         return {'id': self.context.__name__,
                 'hostname': self.context.hostname,