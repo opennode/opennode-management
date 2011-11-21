opennode/opennode-management

commit 8163ec4207eecfba2d299ea2464273bd6ad9c3c7

Fixed an out-of-sync duplicated code bug with model schemas that need to be adapted to and removed the duplication

opennode/oms/endpoint/ssh/cmd/commands.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import time
-from collections import OrderedDict
 
 import transaction
 import zope.schema
@@ -22,7 +21,7 @@ from opennode.oms.model.model.base import IContainer, IIncomplete
 from opennode.oms.model.model.bin import ICommand
 from opennode.oms.model.model.proc import Proc
 from opennode.oms.model.model.symlink import Symlink, follow_symlinks
-from opennode.oms.model.schema import Path, get_schema_fields
+from opennode.oms.model.schema import Path, get_schema_fields, model_to_dict
 from opennode.oms.model.traversal import canonical_path
 from opennode.oms.zodb import db
 
@@ -230,11 +229,7 @@ class CatObjectCmd(Cmd):
                 self._do_cat(obj)
 
     def _do_cat(self, obj):
-        data = OrderedDict()
-        for name, field, schema in get_schema_fields(obj):
-            key = field.description or field.title
-            key = key.encode('utf8')
-            data[key] = field.get(schema(obj))
+        data = model_to_dict(obj)
 
         if data:
             max_key_len = max(len(key) for key in data)

opennode/oms/endpoint/httprest/view.py
@@ -1,5 +1,4 @@
 import json
-from collections import OrderedDict
 
 from grokcore.component import context
 from zope.component import queryAdapter
@@ -15,22 +14,16 @@ from opennode.oms.model.model.filtrable import IFiltrable
 from opennode.oms.model.model.hangar import Hangar
 from opennode.oms.model.model.symlink import follow_symlinks
 from opennode.oms.model.model.virtualizationcontainer import VirtualizationContainer
-from opennode.oms.model.schema import get_schema_fields
+from opennode.oms.model.schema import model_to_dict
 
 
 class DefaultView(HttpRestView):
     context(object)
 
     def render_GET(self, request):
-        obj = self.context
-        # NODE: code copied from commands.py:CatCmd
+        data = model_to_dict(self.context)
 
-        data = OrderedDict()
-        for key, field in get_schema_fields(obj):
-            key = key.encode('utf8')
-            data[key] = field.get(obj)
-
-        data['id'] = obj.__name__
+        data['id'] = self.context.__name__
         data['__type__'] = type(self.context).__name__
 
         return data

opennode/oms/model/schema.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from grokcore.component import context, Adapter, baseclass
 from zope.component import getSiteManager, implementedBy
 from zope.interface import implements
@@ -45,3 +47,11 @@ class ListFromUnicode(CollectionFromUnicode):
 
 class SetFromUnicode(CollectionFromUnicode):
     context(Set)
+
+
+def model_to_dict(obj):
+    data = OrderedDict()
+    for key, field, schema in get_schema_fields(obj):
+        key = key.encode('utf8')
+        data[key] = field.get(schema(obj))
+    return data

commit b6f7d81efef5125afdccb968090d35998b65344d

Fixed unicode issue introduced in 8163ec42, closes #73

opennode/oms/endpoint/ssh/cmd/commands.py
@@ -238,7 +238,7 @@ class CatObjectCmd(Cmd):
                     pretty_value = ', '.join(str(i) for i in value)
                 else:
                     pretty_value = value
-                self.write("%s\t%s\n" % ((key + ':').ljust(max_key_len),
+                self.write("%s\t%s\n" % ((key.encode('utf8') + ':').ljust(max_key_len),
                                          str(pretty_value).encode('utf8')))
 
         if IIncomplete.providedBy(obj):