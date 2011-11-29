0d788084add127f71cd5377e9098c9974761eca9
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-11-29 17:34:28 +0100

    Enabled twisted deferred debug mode

--- twistd-autoreload.py
+++ twistd-autoreload.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from twisted.scripts import twistd
 from pyutils import autoreload
+from twisted.internet import defer
+
+defer.setDebugging(True)
 
 autoreload.main(twistd.run)
