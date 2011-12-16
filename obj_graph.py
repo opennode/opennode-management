f332258eff77e330a4a256d82f5f2d0be06a2e2e
Ilja Livenson <ilja.livenson@gmail.com>
2011-12-16 20:19:42 +0100

    script for generating dot file of current OMS objects

---
+++ obj_graph.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from opennode.oms import setup_environ
+from opennode.oms.zodb import db
+from opennode.oms.model.model import stream, symlink
+
+setup_environ()
+
+dbroot = db.get_root()
+oms_root = dbroot['oms_root']
+
+ignored_classes = [stream.TransientStreamModel]
+
+def build_chart(start, chart_fnm='/tmp/oms-chart.dot'):
+    '''iterative breadth first search from start'''
+    q=[start]
+    processed = set()
+    path = []
+    with open(chart_fnm, 'w') as chart:
+        chart.write("digraph OMS {\n")
+        while q:
+            v=q.pop(0)
+            if not v in processed:
+                if type(v) not in ignored_classes:
+                    chart.write('%s [label="%s"];\n' %(id(v), v.__name__))
+                    if type(v) ==  symlink.Symlink:
+                        chart.write('%s -> %s [style=dotted]; \n' %(id(v), id(v.target)))
+                    if hasattr(v, 'content'):
+                        vals = v.content().values()
+                        for i in vals:
+                            chart.write("%s -> %s;\n" % (id(v), id(i)))
+                        q.extend(vals)
+                    path=path+[v]
+                processed.add(v)
+        chart.write("}\n")
+    return path
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print "Usage: %s output_file_name" % sys.argv[0]
+    else:
+        output_file = sys.argv[1]
+        build_chart(oms_root, output_file)
