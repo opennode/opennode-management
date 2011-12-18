f4c256834294204805de3daca986c87e3c8e957b
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-12-18 16:12:58 +0100

    Added comment for XXX fixes.

--- opennode/oms/backend/func/__init__.py
+++ opennode/oms/backend/func/__init__.py
@@ -116,6 +116,7 @@ class SyncFuncExecutor(FuncExecutor):
                 return res
 
         self.deferred = spawn_func()
+        # XXX: after switching to the 'synchronous' func execute, this is broken, TODO check why!
         # Proc.register(self.deferred, "/bin/func '%s' call %s %s" % (self.hostname.encode('utf-8'),
         #                                                             self.func_action,
         #                                                             ' '.join(str(i) for i in args)))
