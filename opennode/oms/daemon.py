opennode/opennode-management

commit 4dc1b5f0f18707c1499ef30642c4fb3281a3c331

Fixed main startup script current directory issue

opennode/oms/daemon.py
@@ -39,7 +39,6 @@ def run():
         raise Exception("cannot find base_dir")
 
     basedir = get_base_dir()
-    os.chdir(basedir)
 
     db = 'db'
 
@@ -56,7 +55,7 @@ def run():
     pm.addProcess('zeo', ['/bin/sh', '-c', '%s -f %s/data.fs -a %s/socket >%s/zeo.log 2>&1' % (runzeo, db, db, db)], env=os.environ)
     pm.startService()
 
-    sys.argv=[sys.argv[0], '-ny', 'opennode/oms.tac']
+    sys.argv=[sys.argv[0], '-ny', '%s/opennode/oms.tac' % (basedir,)]
 
     if args and args.d:
         # HACK, this code is invoked a second time by autoreload.main

commit 7fb389acc378d172af0a237d153cbf15f5853246

Fixed `omsd -d` issue introduced in 4dc1b5f

opennode/oms/daemon.py
@@ -13,12 +13,21 @@ from twisted.runner.procmon import ProcessMonitor
 def run():
     """Starts the child zeo process and then starts the twisted reactor."""
 
+    def get_base_dir():
+        for i in opennode.__path__:
+            base_dir = os.path.dirname(i)
+            if os.path.exists(os.path.join(base_dir, 'opennode/oms.tac')):
+                return base_dir
+        raise Exception("cannot find base_dir")
+
+    basedir = get_base_dir()
+
     # HACK:autoreload will rerun this executable
     # we have to avoid reparsing the omsd commandlines
     # since we patched sys.argv for hooking into twistd.run
     args = None
     development = False
-    if sys.argv[1:3] != ['-ny', 'opennode/oms.tac']:
+    if sys.argv[1:3] != ['-ny', '%s/opennode/oms.tac' % basedir]:
         parser = argparse.ArgumentParser(description='Start OMS')
         parser.add_argument('-d', action='store_true',
                             help='start in development mode with autorestart')
@@ -30,15 +39,6 @@ def run():
             sys.argv.remove('-d')
 
         args = collections.namedtuple('args', ['d'])(development)
-
-    def get_base_dir():
-        for i in opennode.__path__:
-            base_dir = os.path.dirname(i)
-            if os.path.exists(os.path.join(base_dir, 'opennode/oms.tac')):
-                return base_dir
-        raise Exception("cannot find base_dir")
-
-    basedir = get_base_dir()
 
     db = 'db'
 