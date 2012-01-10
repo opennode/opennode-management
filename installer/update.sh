6f25ace8ec273c23a2e6e487a89e5f1782ffa86f
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-10 11:44:07 +0100

    Added embedded core deps to installer

--- installer/update.sh
+++ installer/update.sh
@@ -1,5 +1,6 @@
-##!/bin/sh
-cd $(dirname $0)
+#!/bin/sh
+DIR=$(dirname $0)
+cd $DIR
 mkdir -p db eggnest
-python bootstrap.py
+python bootstrap.py --setup-source=$DIR/download-cache/ez_setup.py --download-base=$DIR/download-cache
 bin/buildout
