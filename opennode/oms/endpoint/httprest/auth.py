opennode/opennode-management

commit 98eb0ee663e31873878cdea2b8d8228b435414a0

Cleaned up, removed initializer at import time

opennode/oms/security/authentication.py
@@ -43,9 +43,11 @@ def ssha_hash(user, password, encoded_password):
     h.update(salt)
     return "{SSHA}" + encode(h.digest() + salt).rstrip()
 
-password_checker = FilePasswordDB(get_config().get('auth', 'passwd_file'), hash=ssha_hash)
-pubkey_checker = InMemoryPublicKeyCheckerDontUse()
-checkers = [password_checker, pubkey_checker]
+
+def checkers():
+    password_checker = FilePasswordDB(get_config().get('auth', 'passwd_file'), hash=ssha_hash)
+    pubkey_checker = InMemoryPublicKeyCheckerDontUse()
+    return [password_checker, pubkey_checker]
 
 
 @subscribe(IApplicationInitializedEvent)

opennode/oms.tac
@@ -34,7 +34,8 @@ def create_ssh_server():
     rlm.sessionFactory = OmsTerminalSession
 
     the_portal = portal.Portal(rlm)
-    for ch in checkers:
+
+    for ch in checkers():
         the_portal.registerChecker(ch)
 
     conch_factory = ConchFactory(the_portal)

commit 4f1e216b9aae2fcf773d12d30d9ca64ce5ed5d50

Fixed issue with singleton checkers introduced in 98eb0ee

opennode/oms/endpoint/httprest/auth.py
@@ -76,7 +76,7 @@ class HttpRestAuthenticationUtility(GlobalUtility):
     def authenticate(self, request, credentials, basic_auth=False):
         avatar = None
         if credentials:
-            for i in checkers:
+            for i in checkers():
                 try:
                     avatar = yield i.requestAvatarId(credentials)
                     break

opennode/oms/security/authentication.py
@@ -19,6 +19,9 @@ from opennode.oms.config import get_config
 from opennode.oms.security.principals import User
 
 
+_checkers = None
+
+
 class AuthenticationUtility:
     implements(IAuthentication)
 
@@ -45,9 +48,12 @@ def ssha_hash(user, password, encoded_password):
 
 
 def checkers():
-    password_checker = FilePasswordDB(get_config().get('auth', 'passwd_file'), hash=ssha_hash)
-    pubkey_checker = InMemoryPublicKeyCheckerDontUse()
-    return [password_checker, pubkey_checker]
+    global _checkers
+    if _checkers == None:
+        password_checker = FilePasswordDB(get_config().get('auth', 'passwd_file'), hash=ssha_hash)
+        pubkey_checker = InMemoryPublicKeyCheckerDontUse()
+        _checkers = [password_checker, pubkey_checker]
+    return _checkers
 
 
 @subscribe(IApplicationInitializedEvent)