c87de94a6059b7be079b4180e5d12f9fa38374ae
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-10 19:02:48 +0100

    Removed start-oms.sh and use only buildout

--- start-oms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DB=`./current_db_dir.sh`
-mkdir -p $DB # just in case
-runzeo -f $DB/data.fs -a $DB/socket > $DB/zeo.log 2>&1 &
-./twistd-autoreload.py -ny opennode/oms.tac
-
-trap "kill 0" SIGINT SIGTERM EXIT
--- twistd-autoreload.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from twisted.scripts import twistd
-from opennode.utils import autoreload
-from twisted.internet import defer
-
-defer.setDebugging(True)
-
-autoreload.main(twistd.run)
