80eb8c956456359de778ba840e945f95837a5638
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-26 17:48:31 +0100

    Cleaned up removed code

--- opennode/oms/model/model/__init__.py
+++ opennode/oms/model/model/__init__.py
@@ -5,11 +5,6 @@ from .news import NewsItem
 
 __all__ = [OmsRoot]
 
-#__all__ = [OmsRoot, Machines, Computes, Compute, Templates, Template, Network, NetworkInterface, Storage, NewsItem, VncConsole, 'creatable_models']
-
-
-#creatable_models = dict((cls.__name__.lower(), cls)
-#                        for cls in [Compute, Template, Network, NetworkInterface, Storage, VirtualizationContainer, Hangar, NewsItem, VncConsole])
 
 creatable_models = dict((cls.__name__.lower(), cls)
                         for cls in [NewsItem])
