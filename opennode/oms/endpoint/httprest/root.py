opennode/opennode-management

commit 0792986ae4f030fdd62852dc352fb853f4acda12

added a conversion to list when encoding python sets into json to avoid errors. Closes #138

opennode/oms/endpoint/httprest/root.py
@@ -151,6 +151,8 @@ class HttpRestServer(resource.Resource):
             # allow views to take full control of output streaming
             if ret != NOT_DONE_YET:
                 def render(obj):
+                    if obj(type) == set:
+                        return list(obj)  # safeguard against dumping sets
                     print "RENDERING ERROR, cannot json serialize", obj
                     raise TypeError
 

commit 98884352efcef402a0823ef84e57e122bfa85eab

Fixed bug introduced in 0792986a (ON-233, ON-391)

opennode/oms/endpoint/httprest/root.py
@@ -150,7 +150,7 @@ class HttpRestServer(resource.Resource):
             # allow views to take full control of output streaming
             if ret != NOT_DONE_YET:
                 def render(obj):
-                    if obj(type) == set:
+                    if type(obj) == set:
                         return list(obj)  # safeguard against dumping sets
                     print "RENDERING ERROR, cannot json serialize", obj
                     raise TypeError