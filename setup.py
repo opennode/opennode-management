26feda0145debc4c4fab758fef3019e6f9f10611
Ilja Livenson <ilja.livenson@gmail.com>
2012-09-09 18:19:28 +0300

    Updated Twisted version requirement to 12.2.0 (ON-374)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires = [
         "setuptools", # Redundant but removes a warning
         "winpdb",
-        "Twisted==11.1.0",
+        "Twisted==12.2.0",
         "pyasn1==0.1.2", # required by Twisted conch
         "transaction==1.3.0",
         "zope.component==3.12",
