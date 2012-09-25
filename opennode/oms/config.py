025eb9656ff0119dc2aa78d25b12522cfec38b16
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-09-25 19:07:45 +0200

    Refactored oms config helper class so that getters with default arguments can be used without having to use the oms specific conf initialization

--- opennode/oms/config.py
+++ opennode/oms/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from ConfigParser import ConfigParser, Error as ConfigKeyError
 from contextlib import closing
@@ -11,7 +12,6 @@ import opennode.oms
 
 _config = None
 _loaded_config_requirements = []
-_cmdline_override = ConfigParser()
 
 
 def get_config():
@@ -64,34 +64,8 @@ def gen_config_file_names(module, name):
     return [i % name for i in ['%s/opennode-%%s.conf' % base_dir, './opennode-%s.conf', '/etc/opennode/opennode-%s.conf']]
 
 
-class OmsConfig(ConfigParser):
+class OmsBaseConfig(ConfigParser):
     NO_DEFAULT = object()
-
-    def __init__(self, config_filenames=NO_DEFAULT):
-        ConfigParser.__init__(self)
-        self.update(config_filenames)
-
-    def update(self, config_filenames=NO_DEFAULT):
-        if config_filenames == self.NO_DEFAULT:
-            conf_requirements = [i for i in querySubscriptions(object(), IRequiredConfigurationFiles) if type(i) not in _loaded_config_requirements]
-
-            config_filenames = []
-            for i in [i.config_file_names() for i in conf_requirements]:
-                config_filenames.extend(i)
-
-            for i in conf_requirements:
-                _loaded_config_requirements.append(type(i))
-
-            # user override must be last
-            config_filenames.append('~/.opennode-oms.conf')
-
-        # XXX: it would be nice to be able to print out these via some cmdline switch to omsd
-        # print "Reading config files", ', '.join(config_filenames)
-        self.read([os.path.expanduser(i) for i in config_filenames])
-
-        with closing(StringIO()) as s:
-            _cmdline_override.write(s)
-            self.readfp(StringIO(s.getvalue()))
 
     def getboolean(self, section, option, default=NO_DEFAULT):
         try:
@@ -128,3 +102,36 @@ class OmsConfig(ConfigParser):
                 return default
             print "CANNOT FIND CONF KEY", section, option
             raise
+
+
+_cmdline_override = OmsBaseConfig()
+
+
+class OmsConfig(OmsBaseConfig):
+    NO_DEFAULT = object()
+
+    def __init__(self, config_filenames=NO_DEFAULT):
+        ConfigParser.__init__(self)
+        self.update(config_filenames)
+
+    def update(self, config_filenames=NO_DEFAULT):
+        if config_filenames == self.NO_DEFAULT:
+            conf_requirements = [i for i in querySubscriptions(object(), IRequiredConfigurationFiles) if type(i) not in _loaded_config_requirements]
+
+            config_filenames = []
+            for i in [i.config_file_names() for i in conf_requirements]:
+                config_filenames.extend(i)
+
+            for i in conf_requirements:
+                _loaded_config_requirements.append(type(i))
+
+            # user override must be last
+            config_filenames.append('~/.opennode-oms.conf')
+
+        # XXX: it would be nice to be able to print out these via some cmdline switch to omsd
+        # print "Reading config files", ', '.join(config_filenames)
+        self.read([os.path.expanduser(i) for i in config_filenames])
+
+        with closing(StringIO()) as s:
+            _cmdline_override.write(s)
+            self.readfp(StringIO(s.getvalue()))
