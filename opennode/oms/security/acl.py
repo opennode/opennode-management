2556de6d099c5c6ebf3bca84d7e8955efe25699f
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-03-23 15:36:58 +0200

    Fixes and improvements to ACL file handling (OMS-281)

--- opennode/oms/security/acl.py
+++ opennode/oms/security/acl.py
@@ -1,10 +1,14 @@
 import logging
+import os
 import transaction
 
+from grokcore.component import subscribe
 from zope.authentication.interfaces import IAuthentication
 from zope.component import getUtility
 from zope.securitypolicy.interfaces import IPrincipalRoleManager
 
+from opennode.oms.core import IApplicationInitializedEvent
+from opennode.oms.config import get_config
 from opennode.oms.model.traversal import traverse_path
 from opennode.oms.security.interaction import new_interaction
 from opennode.oms.security.permissions import Role
@@ -18,23 +22,41 @@ class NoSuchPermission(Exception):
     pass
 
 
-def preload_acl_file(stream):
+@subscribe(IApplicationInitializedEvent)
+def setup_permissions(event):
+    if event.test:
+        preload_acl_file('')
+        return
+
+    acl_file = get_config().getstring('auth', 'acl_file', 'oms_acl')
+    if not os.path.exists(acl_file):
+        log.warning("ACL file doesn't exist")
+        return
+
+    preload_acl_file(file(acl_file), filename=acl_file)
+
+
+@db.ro_transact
+def preload_acl_file(iterable, filename=''):
     log.info('Preloading ACL rules...')
     lineno = 0
     try:
-        for line in stream:
-            path, permspec = line.split(':', 1)
+        for line in iterable:
+            specline = line.split('#', 1)[0]
+            if not specline:
+                continue
+            path, permspec = specline.split(':', 1)
             lineno += 1
             preload_acl_line(path, permspec)
         transaction.commit()
     except NoSuchPermission as e:
-        log.error('No such permission: %s line: %s' % (e, line))
+        log.error('No such permission: \'%s\'; file: \'%s\' line: %s' % (e, filename, lineno))
+        log.info('Available permissions: %s' % Role.nick_to_role.keys())
         transaction.abort()
 
 
-@db.ro_transact
 def preload_acl_line(path, permspec, meta={}):
-    obj = traverse_path(db.get_root()['oms_root'], path[1:])
+    obj = traverse_path(db.get_root()['oms_root'], path[1:])[0][0]
     auth = getUtility(IAuthentication, context=None)
     interaction = new_interaction(auth.getPrincipal('root'))
     with interaction:
@@ -43,17 +65,14 @@ def preload_acl_line(path, permspec, meta={}):
                       'deny': prinrole.removeRoleFromPrincipal,
                       'unset': prinrole.unsetRoleForPrincipal}
 
-        permtype, kind, principal, perms = permspec.split('#', 1)[0].strip().split(':', 3)
+        permtype, kind, principal, perms = permspec.strip().split(':', 3)
 
         if not perms:
             return
-
-        if kind == 'g':
-            principal = 'g:' + principal
 
         for perm in perms.strip().split(','):
             if perm not in Role.nick_to_role:
                 raise NoSuchPermission(perm)
             role = Role.nick_to_role[perm].id
-            log.info('  %s %s on %s to %s' % (permtype, perm, obj, principal))
+            log.info('%s \'%s\' on %s (%s) to \'%s\'' % (permtype, perm, path, obj, principal))
             action_map[permtype](role, principal)
