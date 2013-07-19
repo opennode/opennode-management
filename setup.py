bf63d2ef5abcf0214d675ca5f31c695dab295d2e
Ilja Livenson <ilja.livenson@gmail.com>
2013-07-20 01:40:30 +0300

    Bumped Twisted version to 13.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
                                         ]},
     install_requires = [
         "setuptools", # Redundant but removes a warning
-        "Twisted==12.3.0",
+        "Twisted==13.1.0",
         "pyasn1==0.1.2", # required by Twisted conch
         "transaction==1.3.0",
         "zope.component==3.12",
