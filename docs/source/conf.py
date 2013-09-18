431f3d1f60f10f694241fb3550bd81c0e6579410
Ilja Livenson <ilja.livenson@gmail.com>
2013-09-18 10:57:49 -0700

    Changed documentation theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@ import sys, os
 import opennode
 from opennode.utils.version import get_git_version
 
+import sphinx_bootstrap_theme
+
 import sphinx.apidoc
 import shutil
 shutil.rmtree('gen', True)
@@ -101,15 +103,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'collapsiblesidebar': True}
+#html_theme_options = {'collapsiblesidebar': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
